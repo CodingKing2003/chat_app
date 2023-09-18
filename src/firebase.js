ec6f6605963ad5bc2301f45cd6efe712221510b5
Rajan Jha <rajanj448@student.sfit.ac.in>
2023-09-18 10:58:39 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC7koLEajSUw5jw0jN-Q7H2YvXqzMBWkBo",
+  authDomain: "chat-app-7ab27.firebaseapp.com",
+  projectId: "chat-app-7ab27",
+  storageBucket: "chat-app-7ab27.appspot.com",
+  messagingSenderId: "615948304871",
+  appId: "1:615948304871:web:b1c2bf3b7f64ccc3e6fd42"
+};
+
+
+export const app = initializeApp(firebaseConfig);
